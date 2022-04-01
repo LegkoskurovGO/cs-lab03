1c5a745b1ac4da4beecc6ffcd52a6a9a631b8e82
LegkoskurovGO <a@d.c>
2022-04-02 00:53:45 +0300

    code, build: Добавлен модуль svg

--- Lab03/main.cpp
+++ Lab03/main.cpp
@@ -1,4 +1,5 @@
 #include "../lab03-histogram/histogram.hpp"
+#include "../lab03-svg/svgFunc.hpp"
 
 #include <iostream>
 #include <vector>
@@ -7,11 +8,6 @@ using namespace std;
 vector<double> input_numbers(size_t count);
 vector<size_t> make_histogram(size_t bin_count, const vector<double>& numbers, double min, double max);
 void show_histogram_text(vector<size_t> bins);
-void show_histogram_svg(const vector<size_t>& bins);
-void svg_begin(double width, double height);
-void svg_end();
-void svg_text(double left, double baseline, string text);
-void svg_rect(double x, double y, double width, double height, string stroke = "#EECCC1", string fill = "#F4BF4F");
 
 int main() {
 
@@ -93,63 +89,4 @@ show_histogram_text(vector<size_t> bins) {
         }
         cout << '\n';
     }
-}
-void
-svg_begin(double width, double height) {
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewBox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
-
-void
-svg_end() {
-    cout << "</svg>\n";
-}
-void
-show_histogram_svg(const vector<size_t>& bins) {
-    
-    const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
-    const auto TEXT_LEFT = 20;
-    const auto TEXT_BASELINE = 20;
-    const auto TEXT_WIDTH = 50;
-    const auto BIN_HEIGHT = 30;
-    const auto BLOCK_WIDTH = 10;
-    const auto MAX_LENGTH_OF_BIN = 35;
-    
-    size_t max_bin = bins[0];
-            for (size_t bin : bins ) {
-                if (bin > max_bin) {
-                    max_bin = bin;
-                }
-            }
-    
-    double top = 0;
-    const bool scaling_needed = max_bin > MAX_LENGTH_OF_BIN;
-    const double scaling_factor = (double)MAX_LENGTH_OF_BIN / max_bin;
-    
-    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    
-    for (size_t bin : bins) {
-        
-        double bin_width = BLOCK_WIDTH * bin;
-        if (scaling_needed) {
-            bin_width *= scaling_factor;
-        }
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
-        top += BIN_HEIGHT;
-    }
-    svg_end();
-}
-void
-svg_text(double left, double baseline, string text) {
-    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
-}
-void
-svg_rect(double x, double y, double width, double height, string stroke, string fill) {
-    cout << "<rect x='" << x << "' y='" << y << "' width='" << width << "' height='" << height << "' stroke='" << stroke << "' fill='" << fill << "' />";
 }
---
+++ lab03-svg/svgFunc.hpp
@@ -0,0 +1,16 @@
+#ifndef svgFunc_hpp
+#define svgFunc_hpp
+
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void show_histogram_svg(const vector<size_t>& bins);
+void svg_begin(double width, double height);
+void svg_end();
+void svg_text(double left, double baseline, string text);
+void svg_rect(double x, double y, double width, double height, string stroke = "#EECCC1", string fill = "#F4BF4F");
+
+#endif /* svgFunc_hpp */
