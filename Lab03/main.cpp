1b7c15e6e1147a908c9335b3b90eb62a2dc5552f
LegkoskurovGO <a@d.c>
2022-05-15 15:30:44 +0300

    code: Выполнен пункт 3.2.7 (При неверно введённых данных выводит текст ошибки)

--- Lab03/main.cpp
+++ Lab03/main.cpp
@@ -40,6 +40,14 @@ main (int argc, char* argv[]) {
           CURLcode res;
           curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
           res = curl_easy_perform(curl);
+            
+            if(res != CURLE_OK) {
+                fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                        curl_easy_strerror(res));
+                curl_easy_cleanup(curl);
+                exit(1);
+            }
+            
           curl_easy_cleanup(curl);
         }
         
